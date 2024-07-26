1a37e428728157343e3665534ff2236b1e523f5e
Paulo César <paulocesarti01@gmail.com>
2024-07-26 12:58:25 -0300

    Update app.module.ts

--- remote/src/app/app.module.ts
+++ remote/src/app/app.module.ts
@@ -5,7 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule
